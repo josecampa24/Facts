7eaa8ad4167ed37d9ea7895a6ba241fe6db2b602
josecampa24 <armandopro224@gmail.com>
2026-07-24 16:47:38 -0600

    refactor button functionality and update form visibility in index.html and style.css

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector(".btn-open");
+btn.addEventListener("click", function () {
+  const form = document.querySelector(".fact-form");
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
